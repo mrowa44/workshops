8e2ef0ae79ddabdf5b5faf401bb79d3333a8bef7
Justyna Rachowicz <justynarachowicz@gmail.com>
2015-06-10 13:02:40 +0200

    Pass product model specs

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :description, presence: true
+  validates :title, presence: true
+  validates :price, presence: true, format: { with: /\A\d+(\.\d{1,2})?\z/ }
+
+  def average_rating
+    reviews.average(:rating)
+  end
 end
